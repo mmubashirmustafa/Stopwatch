d4d8418339d4575963517a09a98f0ca0cddc710d
mmubashirmustafa <64399460+mmubashirmustafa@users.noreply.github.com>
2020-07-11 12:11:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,97 @@
-
-var min = 0;
-var sec = 0;
-var msec = 0;
-var minHeading = document.getElementById("min");
-var secHeading = document.getElementById("sec");
-var msecHeading = document.getElementById("msec");
-var  interval;
-
-function timer(){
-    msec++
-    msecHeading,innerHTML = msec
-    if(msec >= 100){
-        sec++
-        secHeading.innerHTML = sec
-        msec = 0;
-    }else if(sec >=60){
-        min++
-        minHeading,innerHTML = min
-        sec = 0
+class State {
+    constructor(startTimestamp, difference, suspended) {
+      this.startTimestamp = startTimestamp;
+      this.difference = difference;
+      this.suspended = suspended;
     }
-}
-
-function start(){
-    interval = setInterval(timer,10)
-}
-
-function stop(){
-    clearInterval(interval)
-}
-
-function reset(){
-    min = 0;
-    sec = 0;
-    msec = 0;
-    minHeading.innerHTML = min
-    secHeading.innerHTML = sec
-    msecHeading.innerHTML = msec
-    stop()
-}
+  
+    static ready() {
+      return new State(null, 0, 0);
+    }
+  }
+  
+  class Stopwatch {
+    constructor(state) {
+      this.state = state;
+      this.requestAnimationId = null;
+      this.handleClickStart = this.handleClickStart.bind(this);
+      document
+        .getElementById("start")
+        .addEventListener("click", this.handleClickStart);
+      this.handleClickStop = this.handleClickStop.bind(this);
+      document
+        .getElementById("stop")
+        .addEventListener("click", this.handleClickStop);
+      this.handleClickReset = this.handleClickReset.bind(this);
+      document
+        .getElementById("reset")
+        .addEventListener("click", this.handleClickReset);
+      this.tick = this.tick.bind(this);
+      this.render();
+    }
+  
+    static ready() {
+      return new Stopwatch(State.ready());
+    }
+  
+    setState(newState) {
+      this.state = { ...this.state, ...newState };
+      this.render();
+    }
+  
+    tick() {
+      this.setState({
+        difference: new Date(new Date() - this.state.startTimestamp)
+      });
+      this.requestAnimationId = requestAnimationFrame(this.tick);
+    }
+  
+    handleClickStart() {
+      if (this.state.startTimestamp) {
+        // Prevent multi clicks on start
+        return;
+      }
+      this.setState({
+        startTimestamp: new Date() - this.state.suspended,
+        suspended: 0
+      });
+      this.requestAnimationId = requestAnimationFrame(this.tick);
+    }
+  
+    handleClickStop() {
+      cancelAnimationFrame(this.requestAnimationId);
+      this.setState({
+        startTimestamp: null,
+        suspended: this.state.difference
+      });
+    }
+  
+    handleClickReset() {
+      cancelAnimationFrame(this.requestAnimationId);
+      this.setState(State.ready());
+    }
+  
+    render() {
+      const { difference } = this.state;
+      const hundredths = (difference
+        ? Math.floor(difference.getMilliseconds() / 10)
+        : 0
+      )
+        .toString()
+        .padStart(2, "0");
+      const seconds = (difference ? Math.floor(difference.getSeconds()) : 0)
+        .toString()
+        .padStart(2, "0");
+      const minutes = (difference ? Math.floor(difference.getMinutes()) : 0)
+        .toString()
+        .padStart(2, "0");
+  
+      // Render screen
+      document.getElementById("minutes").textContent = minutes;
+      document.getElementById("seconds").textContent = seconds;
+      document.getElementById("hundredths").textContent = hundredths;
+    }
+  }
+  
+  const STOPWATCH = Stopwatch.ready();
+  
